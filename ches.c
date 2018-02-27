ed76560cc85ee727853123db84c4617e68efc2bb
Скирда Андрей Александрович <ip711s24@mwp01.csc.local>
2018-02-27 12:30:02 +0700

    Board output function

---
+++ ches.c
@@ -0,0 +1,51 @@
+#include<stdlib.h>
+
+#include<stdio.h>
+
+int main()
+
+{
+
+    printf(" |a|b|c|d|e|f|g|h|\n");
+
+    printf("___________________\n");
+
+    printf("8|r|n|b|q|k|b|n|r|8\n");
+
+    printf("___________________\n");
+
+    printf("7|p|p|p|p|p|p|p|p|7\n");
+
+    printf("___________________\n");
+
+    printf("6| | | | | | | | |6\n");
+
+    printf("___________________\n");
+
+    printf("5| | | | | | | | |5\n");
+
+    printf("___________________\n");
+
+    printf("4| | | | | | | | |4\n");
+
+    printf("___________________\n");
+
+    printf("3| | | | | | | | |3\n");
+
+    printf("___________________\n");
+
+    printf("2|p|p|p|p|p|p|p|p|2\n");
+
+    printf("___________________\n");
+
+    printf("1|r|n|b|q|k|b|n|r|1\n");
+
+    printf("___________________\n");
+
+    printf(" |a|b|c|d|e|f|g|h|\n");
+
+    system("PAUSE");
+
+    return 0;
+
+}
